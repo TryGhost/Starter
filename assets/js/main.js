ba97e0c1482b9170c7a12607bec5fa9f283e449a
Aditya Kundurkar <95044988+AK46rocks@users.noreply.github.com>
2024-10-22 09:27:13 +0100

    Updated depoly-theme.yml file (#2)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+import "../css/main.css";
+
 // (function () {
 //     pagination(true);
 // })();
--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,13 @@
 import { defineConfig } from 'rollup';
-// A Rollup plugin which locates modules using the Node resolution algorithm
+// Plugins
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-// A Rollup plugin to convert CommonJS modules to ES6, so they can be included in a Rollup bundle
 import commonjs from '@rollup/plugin-commonjs';
-// Use the latest JS features in your Rollup bundle
 import babel from '@rollup/plugin-babel';
-// Minifies the bundle
 import terser from '@rollup/plugin-terser';
-
-// CSS
-// Enable the PostCSS preprocessor
 import postcss from 'rollup-plugin-postcss';
-// Use @import to include other CSS files
 import atImport from 'postcss-import';
-// Use the latest CSS features in your Rollup bundle
 import postcssPresetEnv from 'postcss-preset-env';
-
-// Development: Enables a livereload server that watches for changes to CSS, JS, and Handlbars files
+import autoprefixer from 'autoprefixer';
 import { resolve } from "path";
 import livereload from 'rollup-plugin-livereload';
 
@@ -27,7 +18,7 @@ export default defineConfig({
         dir: "assets/built",
         sourcemap: true,
         format: 'iife',
-        plugins: [terser()]
+        plugins: [process.env.BUILD === "production" && terser()]
     },
     plugins: [
         commonjs(),
@@ -37,15 +28,16 @@ export default defineConfig({
             extract: true,
             sourceMap: true,
             plugins: [
-                atImport(),
-                postcssPresetEnv({})
+                atImport(),                // Allows @import to work in your CSS
+                postcssPresetEnv(),        // Enables modern CSS features
+                autoprefixer()             // Adds vendor prefixes for cross-browser compatibility
             ],
-            minimize: true,
+            minimize: process.env.BUILD === "production",  // Minify in production
         }),
         process.env.BUILD !== "production" && livereload({
-            watch: resolve('.'),
-            extraExts: ['hbs'],
-            exclusions: [resolve('node_modules')]
+            watch: resolve('assets'),   // Watch the 'assets' folder for changes
+            extraExts: ['hbs'],         // Add Handlebars files (.hbs) to be watched
+            exclusions: [resolve('node_modules')]  // Exclude the 'node_modules' folder from being watched
         }),
     ]
-})
+});
